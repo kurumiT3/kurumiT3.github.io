61f1027d4a2db5c923ef06e37ff217213438a6a0
kurumit3 <1817855234@qq.com>
2020-09-19 11:32:04 +0800

    Site updated: 2020-09-19 11:32:04

---
+++ more/catch/webpack.config.js
@@ -0,0 +1,64 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const babelLoader = {
+  loader: 'babel-loader',
+  options: {
+    cacheDirectory: true,
+    presets: [
+      '@babel/preset-env'
+    ]
+  }
+};
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'catch-the-cat.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          babelLoader,
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          babelLoader
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'raw-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: 'public'
+      },
+      {
+        from: 'node_modules/phaser/dist/phaser.min.js'
+      }
+    ])
+  ],
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
